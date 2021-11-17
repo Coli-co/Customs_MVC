8560313250804cf78af2497c4d0abf0b44254bbe
Coli <awestlifeblue@gmail.com>
2021-11-17 14:21:13 +0800

    First App

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello world")
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+  console.log("user hit the server")
+  res.send('Custom Page')
+})
+
+
+app.listen(5000, () => {
+  console.log('server is listening on port 5000...')
+})
