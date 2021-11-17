9230d1ad26196216c693d9cbbec0d9da9bd1c6a8
Coli <awestlifeblue@gmail.com>
2021-11-17 15:20:10 +0800

    Customer Data App

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
-
+const {customer} = require('./data')
 
 app.get('/', (req, res) => {
   console.log("user hit the server")
-  res.send('Custom Page')
+  res.status(200).send('Home Page')
+})
+
+
+app.get('/customer', (req, res) => {
+  // res.status(200).send('Customer Page')
+  res.json(customer)
+})
+
+
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>Page not found</h1>')
 })
 
 
---
+++ data.js
@@ -0,0 +1,60 @@
+const customer = [
+  {
+    customer_id: 1,
+    first_name: 'Babara',
+    last_name: 'MacCaffrey',
+    birth_date: '1986/3/28',
+    phone: '781-932-9754',
+    address: '0 Sage Terrace',
+    city: 'Waltham',
+    state: 'VA',
+    points: 2373
+  },
+  {
+    customer_id: 2,
+    first_name: 'Ines',
+    last_name: 'Brushfield',
+    birth_date: '1986/4/13',
+    phone: '804-427-9456',
+    address: '14187 Commercial Trail',
+    city: 'Hampton',
+    state: 'VA',
+    points: 1047
+  },
+  {
+    customer_id: 3,
+    first_name: 'Freddi',
+    last_name: 'Boagey',
+    birth_date: '1985/2/7',
+    phone: '719-724-7869',
+    address: '251 Springs Junction',
+    city: 'Colorado Springs',
+    state: 'CO',
+    points: 2967
+  },
+  {
+    customer_id: 4,
+    first_name: 'Ambur',
+    last_name: 'Roseburgh',
+    birth_date: '1974/4/14',
+    phone: '407-231-8017',
+    address: '30 Arapahoe Terrace',
+    city: 'Orlando',
+    state: 'FL',
+    points: 457
+  },
+  {
+    customer_id: 5,
+    first_name: 'Clemmie',
+    last_name: 'Betchley',
+    birth_date: '1973/11/7',
+    phone: NaN,
+    address: '5 Spohn Circle',
+    city: 'Arlington',
+    state: 'TX',
+    points: 3675
+  }
+]
+
+
+module.exports = {customer}
